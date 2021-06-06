7840f77f661284da91f3da109df37d00b65c77c6
luck <1569717859@qq.com>
2021-06-06 15:29:18 +0800

    master

---
+++ src/views/home/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar.vue'
+import HomeSwiper from './childComps/HomeSwiper.vue'
+import RecommendView from './childComps/RecommendView.vue'
+
+
+import {getHomeMultidata} from '../../network/home'
+
+
+export default {
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    }
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    }).catch((err) => {
+      
+    });
+  },
+}
+</script>
+
+<style>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
+
+</style>
